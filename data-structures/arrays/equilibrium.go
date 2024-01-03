79dfab79668ce0f4b9cb85455933e08d9731ae07
agusanggarwanto <agusanggarwanto@gmail.com>
2024-01-03 18:23:44 +0700

    godoc: Equilbirum

--- data-structures/arrays/equilibrium.go
+++ data-structures/arrays/equilibrium.go
@@ -9,6 +9,10 @@ func sum(arr []int) int {
 	return sum
 }
 
+// Equilibrium finds the index of an element in the given array where the sum of the elements to the left of the index is equal to the sum of the elements to the right of the index.
+//
+// arr: the input array of integers
+// Returns the index of the equilibrium point if found, otherwise -1.
 func Equilibrium(arr []int) int {
 
 	var leftSum int
